8b35fd2f6a34169be8e35b0701d8d7335398272b
Christine Koppelt <ch.ko123@googlemail.com>
2013-03-28 00:09:04 +0100

    add line breaks and remove css class

--- calendar/calendar_generator.py
+++ calendar/calendar_generator.py
@@ -19,16 +19,17 @@ def write_calendar ():
         labels.append("</div>")
         if month % 3 == 0:
             labels.append("</div>")
+        labels.append('\n')
 
 
 def write_month (month):
     month_name = calendar.month_name[month].decode("utf8")
     href = '/events/' + str(CURRENT_YEAR) + '/' + str(month) + '/'
-    labels.append('<a class="month_title_link" href="' + href + '">' + month_name.lower() + '</a>')
-    labels.append('<br/>')
+    labels.append('<div><a class="month_title_link" href="' + href + '">' + month_name.lower() + '</a></div>')
     cal = calendar.Calendar ()
     for day in cal.itermonthdays(CURRENT_YEAR, month):
         write_day (month, day)
+    
 
 
 def write_day (month, day):
@@ -38,8 +39,10 @@ def write_day (month, day):
             str_day = '0' + str_day
         current_weekday = calendar.weekday(CURRENT_YEAR, month, day)
         href = '/events/' + str(CURRENT_YEAR) + '/' + str(month) + '/' + str(day) + '/'
+
         attr_class = get_day_class_attribute(CURRENT_YEAR, month, day, current_weekday)
-        labels.append('<a class="' + attr_class + '" href="' + href + '">' + str_day + '</a>')
+
+        labels.append('<a' + attr_class + ' href="' + href + '">' + str_day + '</a>')
         if current_weekday == 6:
            labels.append('<br/>')
     else:
@@ -47,17 +50,19 @@ def write_day (month, day):
 
 
 def get_day_class_attribute (year, month, day, current_weekday):
-    str_year = str(year)
-    str_month = str(month)
-    if (month < 10):
-            str_month = '0' + str_month
-    str_day = str(day)
-    if (day < 10):
-            str_day = '0' + str_day
-    attr_class = 'd' + str_year + str_month + str_day
+    #str_year = str(year)
+    #str_month = str(month)
+    #if (month < 10):
+    #        str_month = '0' + str_month
+    #str_day = str(day)
+    #if (day < 10):
+    #        str_day = '0' + str_day
+    #attr_class = 'd' + str_year + str_month + str_day
     if current_weekday == 6 or current_weekday == 5:
-        attr_class = attr_class + " day_weekend"
-    return attr_class
+        attr_class = "day_weekend"
+        return ' class="' + attr_class + '"'
+    else:
+        return '';
 
 
 with codecs.open('calendarfragment.html', 'w', 'utf-8') as f:
