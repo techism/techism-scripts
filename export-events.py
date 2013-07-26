02e286d6121a1b465a869e3560ee5b85542e9d4f
Stefan Seelmann <mail@stefan-seelmann.de>
2013-07-26 08:38:56 +0200

    Add event export script

---
+++ export-events.py
@@ -0,0 +1,32 @@
+from techism.models import Event
+import csv
+
+with open('techism-events.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    
+    headers = [
+        'title',
+        'date_time_begin',
+        'date_time_end',
+        'url',
+        'location_name',
+        'location_lat',
+        'location_lng',
+        'tags',
+    ]
+    writer.writerow(headers)
+    
+    events = Event.objects.filter(published=True).order_by('date_time_begin', 'id').prefetch_related('tags').prefetch_related('location').all().iterator()
+    for event in events:
+        row = [
+            event.title.encode('utf-8'),
+            event.date_time_begin.isoformat() if event.date_time_begin else '',
+            event.date_time_end.isoformat() if event.date_time_end else '',
+            event.url.encode('utf-8') if event.url else '',
+            event.location.name.encode('utf-8') if event.location else '',
+            event.location.latitude if event.location else '',
+            event.location.longitude if event.location else '',
+            '|'.join([tag.name.encode('utf-8') for tag in event.tags.all()]) if event.tags else ''
+        ]
+        writer.writerow(row)
+
